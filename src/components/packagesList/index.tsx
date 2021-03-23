d19b30043da2f743d467199d2c705176856bc344
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-03-23 19:05:16 +0530

    hooks added

--- src/components/packagesList/index.tsx
+++ src/components/packagesList/index.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "../../hooks/useActions";
+
 const PackagesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const repositories = useSelector((state) => state);
+  const { searchRepositories } = useActions();
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+      searchRepositories(term)
+  };
   return (
     <>
-      <form action="">
-          <input type="text"/>
-          <button>Search</button>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <button>Search</button>
       </form>
+      <div>
+        <pre>{JSON.stringify(repositories, undefined, 2)}</pre>
+      </div>
     </>
   );
 };
